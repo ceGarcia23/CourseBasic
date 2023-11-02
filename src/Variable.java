af692b2e35b544ae90cde5ffd54bb5202ee7881b
Tu Nombre <tu@email.com>
2023-11-01 22:56:31 -0500

    FEAT: variable age

--- src/Variable.java
+++ src/Variable.java
@@ -3,11 +3,24 @@ public class Variable {
     public static void main(String[] args) {
         //Todo Auto-generated method stub
 
-        byte age;
+        byte age; // Declaracion de la variable edad de tipo byte
 
         age=35;
 
+        /* Instrucción imprime en la
+        consola el valor de
+        la variable edad.
+         */
         System.out.println(age);
+
+        int car=40;
+
+        car=45;
+
+        System.out.println(car);
+
+
+
 
 
 
