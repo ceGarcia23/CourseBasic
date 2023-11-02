09828c8b592d024b2e36d22c2d18f272a3a1a902
Tu Nombre <tu@email.com>
2023-11-02 18:49:19 -0500

    ADD: variable final

---
+++ src/DeclarationOperatorsConstants.java
@@ -0,0 +1,24 @@
+public class DeclarationOperatorsConstants {
+
+    public static void main(String[] args) {
+
+
+        int oper1, oper2, total;
+        oper1 = 8;
+        oper2 = 7;
+        total = oper1 + oper2;
+
+        System.out.println(total);
+
+        int c = 5;
+
+        final double apulgadas = 5;
+
+        double cm = 6;
+
+        double resultado = cm / apulgadas;
+
+        System.out.println("En " + cm + " cm hay " + resultado + " pulgadas");
+
+    }
+}
