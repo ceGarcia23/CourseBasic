27b07ef315bcf3d1dd7a4c946d89d5e44f297868
Tu Nombre <tu@email.com>
2023-11-02 19:10:51 -0500

    ADD:Class NumericaCalculations

---
+++ src/NumericalCalculations.java
@@ -0,0 +1,15 @@
+public class NumericalCalculations {
+
+    public static void main(String[] args) {
+
+        // Class MATH
+
+        double raiz = Math.sqrt(9);
+
+        System.out.println(raiz);
+
+
+
+
+    }
+}
